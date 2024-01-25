49038c42d9315db38a42eb7d46a00c34b3da7056
AGEN233 <AGEN233@hotamil.com>
2024-01-25 14:39:08 +0800

    Site updated: 2024-01-25 14:39:07

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+    "CONFIG": {
+        "modules": "01234",
+        "background": "5",
+        "tmpColor": "FFFFFF",
+        "tmpSize": "16",
+        "cityColor": "FFFFFF",
+        "citySize": "16",
+        "aqiColor": "FFFFFF",
+        "aqiSize": "16",
+        "weatherIconSize": "24",
+        "alertIconSize": "18",
+        "padding": "10px 10px 10px 10px",
+        "shadow": "0",
+        "language": "auto",
+        "fixed": "false",
+        "vertical": "top",
+        "horizontal": "left",
+        "city": "CN101281104",
+        "key": "9b3fe71fca254225b2378656edaa6173"
+    }
+}
