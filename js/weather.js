e8bf2be4e106608b3940b1f6ac2fa116b5e8f4a2
AGEN233 <AGEN233@hotmail.com>
2024-02-17 20:04:48 +0800

    Site updated: 2024-02-17 20:04:46

---
+++ js/weather.js
@@ -0,0 +1,24 @@
+WIDGET = {
+    "CONFIG": {
+        "modules": "0124",
+        "background": "5",
+        "tmpColor": "FFFFFF",
+        "tmpSize": "16",
+        "cityColor": "FFFFFF",
+        "citySize": "16",
+        "aqiColor": "FFFFFF",
+        "aqiSize": "16",
+        "weatherIconSize": "24",
+        "alertIconSize": "18",
+        "padding": "10px 10px 10px 10px",
+        "shadow": "0",
+        "language": "auto",
+        "fixed": "true",
+        "vertical": "top",
+        "horizontal": "left",
+        "city": "CN101281104",
+        "left": "225",
+        "top": "10",
+        "key": "d9e86df62a34475e8710da3562929100"
+    }
+}
