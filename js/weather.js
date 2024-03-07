23f8292695e63b86f3c54f812ecb693b9f9706ed
AGEN233 <AGEN233@hotmail.com>
2024-03-07 16:28:12 +0800

    Site updated: 2024-03-07 16:28:10

--- js/weather.js
+++ js/weather.js
@@ -1,24 +1,22 @@
-WIDGET = {
-    "CONFIG": {
+    WIDGET = {
+        "CONFIG": {
         "modules": "0124",
-        "background": "5",
-        "tmpColor": "FFFFFF",
-        "tmpSize": "16",
-        "cityColor": "FFFFFF",
-        "citySize": "16",
-        "aqiColor": "FFFFFF",
-        "aqiSize": "16",
-        "weatherIconSize": "24",
-        "alertIconSize": "18",
-        "padding": "10px 10px 10px 10px",
-        "shadow": "0",
-        "language": "auto",
-        "fixed": "true",
-        "vertical": "top",
-        "horizontal": "left",
-        "city": "CN101281104",
-        "left": "225",
-        "top": "10",
-        "key": "d9e86df62a34475e8710da3562929100"
+    "background": "5",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "1",
+    "language": "auto",
+    "fixed": "false",
+    "vertical": "top",
+    "horizontal": "right",
+    "city": "CN101281104",
+    "key": "ab7d71d65e9d40968195e874a0a239a7"
     }
 }
